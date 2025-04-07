20a9e1465ef5a1692f6d72632103ed02c0a0b048
mohamedj0910 <mohamedj0910@gmail.com>
2025-04-07 13:26:18 +0530

    Header component name fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
